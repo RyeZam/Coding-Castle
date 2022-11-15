109bb5260a9fe26da22333eb9f210e88bba96998
RyeZam <zryanlester@gmail.com>
2022-11-16 01:01:05 +0800

    tilemaps

---
+++ Assets/SuperTiled2Unity/Imports/full spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="full spritesheet" tilewidth="16" tileheight="16" tilecount="252" columns="28">
+ <image source="full spritesheet.png" width="448" height="144"/>
+</tileset>
