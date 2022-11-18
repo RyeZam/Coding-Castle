62705d3eed1b449606c46e8b8fe9cb70770cd335
RyeZam <zryanlester@gmail.com>
2022-11-18 23:22:56 +0800

    levels_9-10

--- Assets/SuperTiled2Unity/Imports/full tilemap.tsx
+++ Assets/SuperTiled2Unity/Imports/full tilemap.tsx
@@ -1,4 +1,93 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="full tilemap" tilewidth="16" tileheight="16" tilecount="72" columns="9">
  <image source="full tilemap.png" width="144" height="128"/>
+ <tile id="39">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.03125" y="8.96875">
+    <polygon points="0.0273438,0.0273438 2.03906,0.0273438 2.03906,1.02344 3.01953,1.01953 3.01563,7.01563 0.03125,7.03125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index" id="4">
+   <object id="3" x="15.913" y="9">
+    <polygon points="0.0777778,0.00555556 0.0869565,7 -2.91208,7.01111 -2.92319,0.994444 -2.45097,0.993478 -1.92222,0.972222 -1.90097,0.0130435"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="48">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 3.95652,0 3.99541,10.0046 3.00821,9.99444 3.01208,16.0056 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="49">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="11.8261" y="-0.0869565">
+    <polygon points="0.144444,0.1 0.202657,11.0889 1.17029,11.0843 1.17488,16.0925 4.17391,16.087 4.17391,0.0869565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="55">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.0156" y="10.0156">
+    <polygon points="0,-0.0333333 -4.01806,-0.0256944 -5.00903,-1.04028 -10.0149,-1.02014 -11.0469,-0.015625 -16.0056,-0.0135417 -16.0156,5.98438 -0.015625,5.98438"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="56">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="10">
+    <polygon points="0,0 4,-0.0625 4.96875,-1.0625 11.9063,-1.0625 13,-0.0159722 15.9951,-0.0625 16,6 0,6"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.96875" y="0">
+    <polygon points="0,0 0.0351563,9 -0.960938,8.98438 -0.96875,15.0039 -1.97266,14.9883 -1.97656,15.9961 -3.96875,16 -3.96875,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="11.9667" y="0">
+    <polygon points="0,0 0.0111111,8.97778 1.01111,8.97778 1.02222,14.9889 2.02222,14.9889 2.02552,16.0039 4.03333,16 4.03333,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0869565" y="9.91304">
+    <polygon points="0,0 4,0.0434783 5.04348,-0.913043 10.2174,-0.913043 11.0435,0 16.0435,0.130435 16.087,6.08696 0.0869565,6.08696"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="64">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.95652" y="-0.0434783">
+    <polygon points="0,0 0.0278533,7.02582 1.0034,7.03465 2.04246,8.02344 7.03906,8.04246 8,9 12,8.95652 12.0435,16.1304 -3.95652,16.0435 -3.95652,0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="65">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.9844" y="9">
+    <polygon points="0,0 -2.97743,-0.015625 -4.01007,-1.03125 -10.991,-1.01354 -11.9854,-0.0145833 -16,-0.00798611 -15.9844,7 0.015625,7"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="68">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="9.04348">
+    <polygon points="0,0 3.86957,-0.0869565 4.86957,-1.08696 10,-1.08696 11.0014,-2.04246 12.0044,-2.06454 11.9956,-9.04586 16,-9.04348 16,6.95652 0,6.95652"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
